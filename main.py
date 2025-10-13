d55877323566503ba22b4ebb46d7847933f70b69
333m <Phattrawin@gmail.com>
2025-10-13 13:05:18 +0700

    fix insert room

--- main.py
+++ main.py
@@ -220,6 +220,17 @@ Enter command : """)
         print("Insert room . . .")
         start_time = time.perf_counter()
         targetRoom = int(targetRoom)
+        arr = []
+        for room in addRoom:
+            arr.append(room)
+        for i in range(hotel.size()):
+            tmp = hotel.dequeue()
+            hotel.enqueue(tmp)
+            arr.append(pow(2,tmp[0]-1)*pow(3,tmp[1]-1)*pow(5,tmp[2]-1)*pow(7,i))
+        end = max(arr)
+        if end >= targetRoom:
+            print(f"Room : {targetRoom} already have")
+            continue
         if targetRoom in delRoom:
             delRoom.remove(targetRoom)
         if not targetRoom in addRoom:
